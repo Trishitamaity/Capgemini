27035fd0d4570f2268579073db543caf7f8be483
Trishita Maity <121662935+Trishitamaity@users.noreply.github.com>
2026-01-13 20:08:19 +0530

    Delete Convert_Case.java

--- Convert_Case.java
+++
@@ -1,9 +0,0 @@
-package Capgemini;
-
-class Convert_Case {
-    public static void main(String[] args) {
-        char ch = 'M';
-        char result = Character.toLowerCase(ch);
-        System.out.println("Converted character: " + result);
-    }
-}
