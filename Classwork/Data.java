a565e0f5f5985931993fd4d2e48ca8163e041d82
Trishita Maity <121662935+Trishitamaity@users.noreply.github.com>
2026-01-19 11:42:04 +0530

    Delete Classwork directory

--- Classwork/Data.java
+++
@@ -1,6 +0,0 @@
-package Classwork;
-
-public class Data {
-	static int num1 = 12321;
-	static int num2 = 45;
-}
--- Classwork/Driver.java
+++
@@ -1,8 +0,0 @@
-package Classwork;
-
-public class Driver {
-	public static void main(String[] args) {
-		Program.palindrome(Data.num1);
-		Program.prime(Data.num2);
-	}
-}
