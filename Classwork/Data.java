8d9b22300adceae6a048f280ed5f57893b1ce353
Trishita Maity <121662935+Trishitamaity@users.noreply.github.com>
2026-01-19 11:43:06 +0530

    Add files via upload

---
+++ Classwork/Data.java
@@ -0,0 +1,6 @@
+package Classwork;
+
+public class Data {
+	static int num1 = 12321;
+	static int num2 = 45;
+}
---
+++ Classwork/Driver.java
@@ -0,0 +1,8 @@
+package Classwork;
+
+public class Driver {
+	public static void main(String[] args) {
+		Program.palindrome(Data.num1);
+		Program.prime(Data.num2);
+	}
+}
