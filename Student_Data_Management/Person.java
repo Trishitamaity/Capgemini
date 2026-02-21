4c0fbc3aefe2c8577985071eb7f3f24f3ef6cd6b
Trishita Maity <121662935+Trishitamaity@users.noreply.github.com>
2026-02-21 10:45:33 +0530

    Add files via upload

---
+++ Student_Data_Management/Person.java
@@ -0,0 +1,18 @@
+package Student_Data_Management;
+
+public class Person {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
